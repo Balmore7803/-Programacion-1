fcc21121861289f860a37a0e295086eac8150203
Balmore <josebalmorerodriguez1999@gmail.com>
2024-08-14 11:10:26 -0600

    PAR O IMPAR

--- Program.cs
+++ Program.cs
@@ -8,21 +8,16 @@ namespace _Progra123
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            //obtener la secuencia fibonacii
-            int a = 0, b = 1, c = 1;
-            /*
-             0+1=1
-            */
-            while (c < 100)
-            {
-                Console.WriteLine("{0}+{1}={2}", a, b, c);
-                b = a;
-                a = c;
-                c = a + b;
-            }
-            Console.ReadLine();
+        static void Main(string[] args) {
+            // determinar si cada numero de la serie es par o impar.
+            int[] serie = new int[] { 5, 9, 4, 6, 3, 2 };
+            foreach (int num in serie)
+    
+            { 
+                Console.WriteLine("El num {0} es {1}", num, num % 2 == 0 ? "Par" : "impar");
+
+        }
+            Console.WriteLine();
         }
     }
 }
